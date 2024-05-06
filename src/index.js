5a3c787b8b4d7d62fa100c65e294f9b311cd29df
ChantellDavis <155310121+ChantellDavis@users.noreply.github.com>
2024-05-05 20:21:20 -0400

    added axios in html file added temp in js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+function displayWeather(response) {
+  let currentTemp = document.querySelector("#current-Temp")
+  let temperature = Math.round (response.data.temperature.current)
+  let currentCity = document.querySelector("#current-City")
+  currentCity.innerHTML =  response.data.city  
+  currentTemp.innerHTML = temperature
+}
+
 function search(event) {
     event.preventDefault();
     let searchFormInput = document.querySelector("#search-Form-Input")
-    let currentCity = document.querySelector("#current-City")
-currentCity.innerHTML = (searchFormInput.value)
-}
+   
 
+let apiKey = "50a8380f4oe8265a54940c506tc9b3e0"
+let city = searchFormInput.value
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=imperial`
+axios.get(apiUrl).then(displayWeather);
+}
 function formatDate(date) {
 let day = date.getDay();
 let hours = date.getHours();
