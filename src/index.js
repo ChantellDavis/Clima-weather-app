05bf3e600cd29d24def8da9b1a77c2be4f780788
ChantellDavis <155310121+ChantellDavis@users.noreply.github.com>
2024-08-15 19:25:30 -0400

    approved on JS file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,53 @@
 function displayWeather(response) {
-  let currentTemp = document.querySelector("#current-Temp")
+  let currentTemp = document.querySelector("#current-Temp");
   let temperature = (response.data.temperature.current);
-  currentTemp.innerHTML = Math.round(temperature)
-  let currentCity = document.querySelector("#current-City")
-  currentCity.innerHTML =  (response.data.city)
+  let currentCity = document.querySelector("#current-City");
   let weatherCondition = document.querySelector("#weather-Condition");
-  weatherCondition.innerHTML = (response.data.condition.description);
   let weatherConditionData = document.querySelector("#weather-Condition-Data");
-  weatherConditionData.innerHTML = `${response.data.temperature.humidity}%` ;
   let weatherConditionDataTwo = document.querySelector("#weather-Condition-Data-Two");
-  weatherConditionDataTwo.innerHTML = `${response.data.wind.speed} Km/h`;
+ let currentDate = document.querySelector("#current-Date");
+  let date = new Date(response.data.time * 1000);
+
+currentDate.innerHTML = formatDate(date);
   let icon = document.querySelector("#icon")
+
   icon.innerHTML = `<img
             src="${response.data.condition.icon_url}"
             class="weatherEmoji"
-            id="weather-Emoji" />`
+            id="weather-Emoji"
+             />`
+
+    currentTemp.innerHTML = Math.round(temperature);
+    currentCity.innerHTML =  (response.data.city);
+     weatherCondition.innerHTML = (response.data.condition.description);
+      weatherConditionData.innerHTML = `${response.data.temperature.humidity}%`;
+       weatherConditionDataTwo.innerHTML = `${Math.round(response.data.wind.speed)} Mph`;
   }
 
+function formatDate(date) {
+let day = date.getDay();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    let minutes = `0${minutes}`
+};
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+
+
+
+
+let formattedDate = days[day];
+return `${formattedDate} ${hours}:${minutes}`
+};
 function searchCity(city) {
 let apiKey = "50a8380f4oe8265a54940c506tc9b3e0"
  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`
@@ -29,34 +60,38 @@ function search(event) {
     searchCity (searchFormInput.value);
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#weatherForecast");
+
+let days = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu"];
+
+let forecastHTML = "";
+days.forEach(function(day) {
+  
 
 
-function formatDate(date) {
-let day = date.getDay();
-let hours = date.getHours();
-let minutes = date.getMinutes();
+forecastHTML = forecastHTML + `
+        <div class="weatherContainer">
+          <ul>
+            <li class="forcastDay">${day}</li>
+            <li class="forecastEmoji">🌤️</li>
+            <li>
+              <strong class="forecastTempMaximum">78°</strong>
+              <span class="forecastTempMinimum">65°</span>
+            </li>
+          </ul>
+        </div>
+        `;
 
-let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-];
+});
 
-if (hours < 10) {
-    let hours = `0${hours}`
+forecastElement.innerHTML = forecastHTML;
+
 };
 
-if (minutes < 10) {
-    let minutes = `0${minutes}`
-};
 
-let formattedDate = days[day];
-return `${formattedDate} ${hours}:${minutes}`
-}
+
+
 
 let searchForm = document.querySelector("#search-Form");
 searchForm.addEventListener("submit",search);
@@ -64,8 +99,7 @@ searchForm.addEventListener("submit",search);
 searchCity ("Queens")
 
 
-let currentDate = document.querySelector("#current-Date");
-let currentTime = new Date();
-currentDate.innerHTML = formatDate(currentTime);
+
+
 
 
